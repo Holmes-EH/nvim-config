9fb125ba17bf0fe9ccc6b2bf76482cbeb5098906
Holmes-EH <s.holmes.eh@gmail.com>
2024-11-10 12:51:15 +0100

    Remove useless keymaps

--- lua/holmes/core/keymaps.lua
+++ lua/holmes/core/keymaps.lua
@@ -6,12 +6,6 @@ keymap.set("n", "<C-j>", [[<Cmd>wincmd j<CR>]], {})
 keymap.set("n", "<C-k>", [[<Cmd>wincmd k<CR>]], {})
 keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]], {})
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
 	keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
