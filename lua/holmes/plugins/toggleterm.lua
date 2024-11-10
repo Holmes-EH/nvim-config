77a4b8f721b0e2be9ed0b8fb6e06b6afe3f6b535
Holmes-EH <s.holmes.eh@gmail.com>
2024-11-10 12:52:58 +0100

    setup toggle float terminal

--- lua/holmes/plugins/toggleterm.lua
+++ lua/holmes/plugins/toggleterm.lua
@@ -11,6 +11,14 @@ return {
 	keys = {
 		{ [[<C-\>]] },
 		{
+			"<leader>tf",
+			function()
+				local count = vim.v.count1
+				require("toggleterm").toggle(count, 0, vim.loop.cwd(), "float")
+			end,
+			desc = "ToggleTerm floating Terminal",
+		},
+		{
 			"<leader>ts",
 			"<cmd>TermSelect<cr>",
 			desc = "Select term",
