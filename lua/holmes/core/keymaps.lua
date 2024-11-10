7b3584ca3f3da6590e476abd67d273b1cbfc5e02
Holmes-EH <s.holmes.eh@gmail.com>
2024-11-10 11:42:38 +0100

    Add window navigation keymaps && display global on which key

--- lua/holmes/core/keymaps.lua
+++ lua/holmes/core/keymaps.lua
@@ -1,1 +1,27 @@
-return {}
+local keymap = vim.keymap -- for conciseness
+
+-- window navigation
+keymap.set("n", "<C-h>", [[<Cmd>wincmd h<CR>]], {})
+keymap.set("n", "<C-j>", [[<Cmd>wincmd j<CR>]], {})
+keymap.set("n", "<C-k>", [[<Cmd>wincmd k<CR>]], {})
+keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]], {})
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+	keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
