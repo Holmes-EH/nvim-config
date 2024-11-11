b9f4024eb1fdb6ae12c0b5c75214395590b3485e
Holmes-EH <s.holmes.eh@gmail.com>
2024-11-11 10:16:11 +0100

    use catpuccin theme

--- lua/holmes/plugins/lualine.lua
+++ lua/holmes/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "tokyonight",
+				-- theme = "catpuccin",
 				disabled_filetypes = {
 					"neo-tree",
 				},
