499fecd7a8003657d1834fb170a8d495e96478d6
Holmes-EH <s.holmes.eh@gmail.com>
2024-11-17 09:42:17 +0100

    clarify split and tabs keymaps

--- lua/holmes/core/keymaps.lua
+++ lua/holmes/core/keymaps.lua
@@ -7,14 +7,17 @@ keymap.set("n", "<C-k>", [[<Cmd>wincmd k<CR>]], {})
 keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]], {})
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>bd<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>s=", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<C-w>c", { desc = "Close current split" })
+keymap.set("n", "<leader>s,", "10<C-w><", { desc = "Decrease current split width by 10" })
+keymap.set("n", "<leader>s.", "10<C-w>>", { desc = "Increase current split width by 10" })
+keymap.set("n", "<leader>s<", "30<C-w><", { desc = "Decrease current split width by 10" })
+keymap.set("n", "<leader>s>", "30<C-w>>", { desc = "Increase current split width by 10" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>ty", "gt", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tt", "gT", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
