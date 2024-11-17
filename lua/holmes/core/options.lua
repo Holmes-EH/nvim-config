2a52488ae72ebad4273a4010ca0edecec81523dc
Holmes-EH <s.holmes.eh@gmail.com>
2024-11-17 13:26:37 +0100

    finish status col setup

--- lua/holmes/core/options.lua
+++ lua/holmes/core/options.lua
@@ -32,19 +32,19 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 -- folds
-vim.opt.foldmethod = "expr"
+opt.foldmethod = "expr"
 
 -- :h vim.treesitter.foldexpr()
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
 -- ref: https://github.com/neovim/neovim/pull/20750
-vim.opt.foldtext = ""
-vim.opt.fillchars:append("fold: ")
+opt.foldtext = ""
+opt.fillchars:append("fold: ")
 
 -- Open all folds by default, zm is not available
-vim.opt.foldlevelstart = 99
+opt.foldlevelstart = 99
 
-vim.opt.foldcolumn = "1"
+opt.foldcolumn = "1"
 
 vim.diagnostic.config({
 	signs = {
--- lua/holmes/plugins/statuscol.lua
+++ lua/holmes/plugins/statuscol.lua
@@ -6,15 +6,14 @@ return {
 	config = function()
 		local builtin = require("statuscol.builtin")
 		require("statuscol").setup({
-			-- configuration goes here, for example:
 			relculright = true,
 			segments = {
-				{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+				{ text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
 				{
 					sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
 					click = "v:lua.ScSa",
 				},
-				{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+				{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
 				{ sign = { namespace = { "gitsigns*" }, auto = true }, click = "v:lua.ScSa" },
 			},
 		})
