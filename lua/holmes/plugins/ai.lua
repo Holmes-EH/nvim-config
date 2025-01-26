e62fedbdac1e6d5df94bd727008b0574894593dd
Holmes-EH <s.holmes.eh@gmail.com>
2025-01-26 12:37:40 +0100

    feat: replace codecompanion with avante

---
+++ lua/holmes/plugins/ai.lua
@@ -0,0 +1,40 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = false,
+	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+	opts = {
+		-- add any opts here
+		provider = "ollama",
+		vendors = {
+			ollama = {
+				__inherited_from = "openai",
+				api_key_name = "",
+				endpoint = "http://127.0.0.1:11434/v1",
+				model = "deepseek-r1:8b",
+			},
+		},
+	},
+
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	dependencies = {
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"echasnovski/mini.pick", -- for file_selector provider mini.pick
+		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+}
